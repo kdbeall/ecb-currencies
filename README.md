60185c374c408a7eda6029ded8fc5d1dea85223a
kdbeall <10102694+kdbeall@users.noreply.github.com>
2018-11-22 09:22:27 -0500

    Update API to return a single object instead of list.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # ecb-currencies
 Retrieves exchange rates from the European Central Bank in a JSON format
+
+
+## Testing
+    npm install http-server -g
+    http-server test/static
+    npm test
--- currencies.js
+++ currencies.js
@@ -1,25 +1,40 @@
-var convert = require('xml-js');
-var fetch = require('node-fetch');
+const convert = require('xml-js')
+const fetch = require('node-fetch')
 
-function currenciesFormatted(body){
-    return {
-        "date" : date(body),
-        "rates" : ratesList(body)
-    }
-}
-function ratesList(body){
-    return convert.xml2js(body, {compact : true})["gesmes:Envelope"]["Cube"]["Cube"]["Cube"]
-    .map(item => item["_attributes"]);
+function checkStatus (res) {
+  if (res.ok) {
+    return res
+  } else {
+    throw Error(res.statusText)
+  }
 }
 
-function date(body){
-    return convert.xml2js(body, {compact : true})["gesmes:Envelope"]["Cube"]["Cube"]["_attributes"]["time"];
+function currenciesFormatted (body) {
+  return {
+    'date': date(body),
+    'rates': rates(body)
+  }
+}
+function rates (body) {
+  let rates = convert.xml2js(body, { compact: true })['gesmes:Envelope']['Cube']['Cube']['Cube']
+    .map(item => item['_attributes'])
+  let result = {}
+  for (let rate of rates) {
+    result[rate.currency] = parseFloat(rate.rate)
+  }
+  return result
 }
 
-function currencyRates(rateURL="https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml"){
-    return fetch(rateURL)
+function date (body) {
+  return convert.xml2js(body, { compact: true })['gesmes:Envelope']['Cube']['Cube']['_attributes']['time']
+}
+
+function currencyRates (rateURL = 'https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml') {
+  return fetch(rateURL)
+    .then(checkStatus)
     .then(res => res.text())
-    .then(body => { return currenciesFormatted(body) });
+    .then(body => { return currenciesFormatted(body) })
+    .catch(err => { throw err })
 }
-    
-module.exports = currencyRates;
+
+module.exports = currencyRates
