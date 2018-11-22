a5044aef4fc15f75540f00cc568246161d1cde9b
kdbeall <10102694+kdbeall@users.noreply.github.com>
2018-11-22 09:22:27 -0500

    Added instructions, testing using jest, and standard code style.

--- currencies.js
+++ currencies.js
@@ -1,25 +1,35 @@
-var convert = require('xml-js');
-var fetch = require('node-fetch');
+var convert = require('xml-js')
+var fetch = require('node-fetch')
 
-function currenciesFormatted(body){
-    return {
-        "date" : date(body),
-        "rates" : ratesList(body)
-    }
-}
-function ratesList(body){
-    return convert.xml2js(body, {compact : true})["gesmes:Envelope"]["Cube"]["Cube"]["Cube"]
-    .map(item => item["_attributes"]);
+function checkStatus (res) {
+  if (res.ok) {
+    return res
+  } else {
+    throw Error(res.statusText)
+  }
 }
 
-function date(body){
-    return convert.xml2js(body, {compact : true})["gesmes:Envelope"]["Cube"]["Cube"]["_attributes"]["time"];
+function currenciesFormatted (body) {
+  return {
+    'date': date(body),
+    'rates': ratesList(body)
+  }
+}
+function ratesList (body) {
+  return convert.xml2js(body, { compact: true })['gesmes:Envelope']['Cube']['Cube']['Cube']
+    .map(item => item['_attributes'])
 }
 
-function currencyRates(rateURL="https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml"){
-    return fetch(rateURL)
+function date (body) {
+  return convert.xml2js(body, { compact: true })['gesmes:Envelope']['Cube']['Cube']['_attributes']['time']
+}
+
+function currencyRates (rateURL = 'https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml') {
+  return fetch(rateURL)
+    .then(checkStatus)
     .then(res => res.text())
-    .then(body => { return currenciesFormatted(body) });
+    .then(body => { return currenciesFormatted(body) })
+    .catch(err => { throw err })
 }
-    
-module.exports = currencyRates;
+
+module.exports = currencyRates
